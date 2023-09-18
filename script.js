07a98c31e8899f1c6ea3f1b08ac07c48b87c2fc5
Your Name <mikachupichu@gmail.com>
2023-09-18 11:07:53 +0300

    Finishing touches

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ scrollToTopButton.addEventListener("click", () => {
         behavior: "smooth" 
     });
 });
+const hireButton = document.getElementById('Hire');
+        hireButton.addEventListener('click', () => {
+            window.location.href = 'file:///home/mmg/Developments/Code/Phase-0/Webpage/Contact.html';
+        });
+const LearnButton = document.getElementById('Learn');
+        LearnButton.addEventListener('click', () => {
+            window.location.href = 'file:///home/mmg/Developments/Code/Phase-0/Webpage/About.html';
+        });
+
+const WhatButton = document.getElementById('WhatsApp');
+        WhatButton.addEventListener('click', () => {
+            window.location.href = 'https://www.whatsapp.com/';
+        });
